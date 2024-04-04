de6a7ceba5e6045a04fd86d9f521c4f1c54de825
koba2264 <s2001k1022@gmail.com>
2024-04-04 20:59:11 +0900

    indexエンドポイントを作成

---
+++ apps/crud/views.py
@@ -0,0 +1,14 @@
+from flask import blueprints, render_template
+
+# Blureprint で crud アプリを生成する
+crud = Blueprint(
+    "crud",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+)
+
+# indexエンドポイントを作成しindex.htmlを返す
+@crud.route("/")
+def index():
+    return render_template("crud/index.html")
